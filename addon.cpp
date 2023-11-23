27f320ba35710bbb356787f2f22b3f119deb66a1
Vincent Villaluna <v.villaluna@arcanys.com>
2023-11-23 15:13:19 +0800

    Simple example using node addons

--- addon.cpp
+++ addon.cpp
@@ -1,31 +1,21 @@
 // Setup reference: https://github.com/nodejs/node-addon-api/blob/main/doc/setup.md
 #include <napi.h>
+#include <cstdlib> // for system() function
 
-// Basic Example Hello World
-Napi::String EnumerateWindows(const Napi::CallbackInfo& info) {
+Napi::String Stop(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
-    return Napi::String::New(env, "World");
+    return Napi::String::New(env, "Recording stopped.");
 }
 
-// Basic Example addition that accepts params from nodejs
-Napi::Value Add(const Napi::CallbackInfo& info) {
+Napi::String Record(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
-    int arg0 = info[0].As<Napi::Number>().DoubleValue();
-    int arg1 = info[1].As<Napi::Number>().DoubleValue();
-    Napi::Number num = Napi::Number::New(env, arg0 + arg1);
-
-    return num;
+    return Napi::String::New(env, "Recording started.");
 }
-
-// @TODO
-// GetRunningWindow() return array of windows
-// StartRecording(windowID) return stream buffer
-// StopRecording() return boolean
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    // Mapping the functions to the nodeJS code
-    exports.Set(Napi::String::New(env, "enumerateWindows"), Napi::Function::New(env, EnumerateWindows));
-    exports.Set(Napi::String::New(env, "add"), Napi::Function::New(env, Add));
+    // Mapping the functions to the Node.js code
+    exports.Set(Napi::String::New(env, "record"), Napi::Function::New(env, Record));
+    exports.Set(Napi::String::New(env, "stop"), Napi::Function::New(env, Stop));
     return exports;
 }
 
--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const addon = require('./build/Release/addon');
-
 try {
-    console.log(addon.enumerateWindows());
-    console.log(addon.add(1,2));
+    const recording = addon.record();
+    console.log(recording);
 
-    // @TODO
-    // const windows = addon.getRunningWindow();
-    // addon.startRecording(windows[0]);
-    // setTimeout(() => {
-    //  addon.stopRecording();
-    // }, 10_000);
-
+    setTimeout(() => {
+        const stopped = addon.stop();
+        console.log(stopped);
+    }, 10_000);
 } catch (e) {
     console.log(e.message);
 }
